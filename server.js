96013ee54af790d2a3bb80a288511cddb5433818
Gustaf Faivre <ggwf01@gmail.com>
2025-01-07 23:16:43 +0100

    Refactor server.js to use path module for file serving and update login.html to load Bootstrap from local and CDN sources

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+"use strict";
+const express = require("express");
+const path = require("path"); // Importera path-modulen
 const app = express();
 const PORT = 3000;
 
+// Middlewares för att hantera statiska filer
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+// Route för login.html
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
 });
 
+// Starta servern
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
